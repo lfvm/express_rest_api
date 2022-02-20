6fa885843e616fc0b045b475d6305b659e4e2bca
lfvm <lfvm200@gmail.com>
2022-02-20 13:09:33 -0600

    Added db config file

---
+++ database/config.js
@@ -0,0 +1,40 @@
+/*
+    En este script se define una funcion que se conectara a una base de datos
+    de mongoDB.
+    Para ello utilizamos el paquete 'mongoose', que ayudara a conectar, guardar datos, etc.
+
+    Con la funcion connect, podemos ingresar a la db utilizando un string con la liga a la db.
+    
+    Ademas se utiliza un diccionario con las opciones de conexion en caso de ser necesarias
+*/
+
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+const dbConnection = async() => {
+
+    try {
+
+        await mongoose.connect( process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("DB online");
+        
+    } catch (error) {
+
+        console.log(error);
+        throw new Error('Error al conectar a la base de datos')
+
+    }
+
+}
+
+
+module.exports = {
+
+    dbConnection
+
+}
